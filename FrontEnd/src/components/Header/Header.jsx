3854742f17eee39679ad05d462397585ce7f54a2
SamarthNaiik <naiksamarth19062003@gmail.com>
2024-04-11 17:48:03 +0530

    React JS Food Delivery App FrontEnd

---
+++ FrontEnd/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, doloribus rerum quas sequi reiciendis animi, commodi perferendis placeat quos nisi ut voluptates non doloremque recusandae perspiciatis. Optio excepturi voluptatum dolorum.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
